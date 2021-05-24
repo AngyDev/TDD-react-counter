7566d1e19eea512129c177bf6eba6a0680170c9b
Angela Busato <busato.angela@gmail.com>
2021-05-24 13:22:50 +0200

    [add] Counter component

--- src/scripts/features/App/App.js
+++ src/scripts/features/App/App.js
@@ -1,13 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Counter from "../../components/Counter/Counter";
 
 export default function App() {
-    const [counter, setCounter] = useState(0);
     return (
         <div>
-            <h1>This is a counter app</h1>
-            <div className="" id="counter-value">{counter}</div>
-            <button className="" id="increment-btn" onClick={() => setCounter(counter + 1)}>Increment</button>
-            <button className="" id="decrement-btn" onClick={() => setCounter(counter - 1)}>Decrement</button>
+            <Counter />
         </div>
     )
 }
