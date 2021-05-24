69da73969400cb7a40fdf92e9e715ac0e8dd96c9
Angela Busato <busato.angela@gmail.com>
2021-05-24 12:04:33 +0200

    [add] tests to the App component

--- src/scripts/features/App/App.test.js
+++ src/scripts/features/App/App.test.js
@@ -4,10 +4,44 @@ import '@testing-library/jest-dom';
 
 import App from "./App";
 
+import { shallow } from 'enzyme';
+
 describe("Counter Testing", () => {
+
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<App />);
+    });
+
     it("render the title of counter", () => {
-        const { getByText } = render(<App />);
+        expect(wrapper.find('h1').text()).toContain("This is a counter app");
+
+        /*const { getByText } = render(<App />);
         const linkElement = getByText("This is a counter app");
-        expect(linkElement).toBeInTheDocument();
-    })
+        expect(linkElement).toBeInTheDocument();*/
+    });
+
+    it("render a button with text of 'increment'", () => {
+        expect(wrapper.find('#increment-btn').text()).toBe('Increment');
+    });
+
+    it("render the initial value of state in a div", () => {
+        expect(wrapper.find('#counter-value').text()).toBe("0");
+    });
+
+    it("render the click event of increment button and increment counter value", () => {
+        wrapper.find("#increment-btn").simulate("click");
+        expect(wrapper.find("#counter-value").text()).toBe("1");
+    });
+
+    it("render a button with text of 'decrement'", () => {
+        expect(wrapper.find('#decrement-btn').text()).toBe('Decrement');
+    });
+
+    it("render the click event of decrement button and decrement counter value", () => {
+        wrapper.find("#increment-btn").simulate("click");
+        expect(wrapper.find("#counter-value").text()).toBe("1");
+        wrapper.find("#decrement-btn").simulate("click");
+        expect(wrapper.find("#counter-value").text()).toBe("0");
+    });
 })
