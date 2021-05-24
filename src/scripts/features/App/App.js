35bd1325ff6b6aedf88830741f3afa9e0003db29
Angela Busato <busato.angela@gmail.com>
2021-05-24 12:04:11 +0200

    [add] buttons to increment and decrement the counter

--- src/scripts/features/App/App.js
+++ src/scripts/features/App/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function App() {
+    const [counter, setCounter] = useState(0);
     return (
         <div>
             <h1>This is a counter app</h1>
+            <div className="" id="counter-value">{counter}</div>
+            <button className="" id="increment-btn" onClick={() => setCounter(counter + 1)}>Increment</button>
+            <button className="" id="decrement-btn" onClick={() => setCounter(counter - 1)}>Decrement</button>
         </div>
     )
 }
