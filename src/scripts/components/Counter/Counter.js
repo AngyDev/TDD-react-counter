6db20ba3a867568be8369d0a76add706f9b52bc7
Angela Busato <busato.angela@gmail.com>
2021-05-24 13:33:19 +0200

    [add] a check to not decrement the counter below zero

--- src/scripts/components/Counter/Counter.js
+++ src/scripts/components/Counter/Counter.js
@@ -3,12 +3,28 @@ import React, { useState } from 'react';
 export default function Counter() {
     const [counter, setCounter] = useState(0);
 
+    /**
+     * Increment the counter
+     */
+    const increment = () => {
+        setCounter(counter + 1);
+    }
+
+    /**
+     * Decrement the counter if it is not 0
+     */
+    const decrement = () => {
+        if (counter !== 0) {
+            setCounter(counter - 1);
+        }
+    }
+
     return (
         <div>
             <h1>This is a counter app</h1>
             <div className="" id="counter-value">{counter}</div>
-            <button className="" id="increment-btn" onClick={() => setCounter(counter + 1)}>Increment</button>
-            <button className="" id="decrement-btn" onClick={() => setCounter(counter - 1)}>Decrement</button>
+            <button className="" id="increment-btn" onClick={increment}>Increment</button>
+            <button className="" id="decrement-btn" onClick={decrement}>Decrement</button>
         </div>
     )
 }
